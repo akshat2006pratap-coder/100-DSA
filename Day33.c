b513224a5e6eb49eb095411932420cbf38224337
Akshat Pratap <akshat2006pratap@gmail.com>
2026-03-05 23:03:48 +0530

    Add files via upload

--- Day33.c
+++ Day33.c
@@ -1,36 +1,63 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <ctype.h>
 
-struct node {
-   int data;
-   struct node *link;
-};
+char stack[100];
+int top = -1;
+
+void push(char x){
+    stack[++top] = x;
+}
+
+char pop(){
+    return stack[top--];
+}
+
+int precedence(char x){
+    if(x == '+' || x == '-') return 1;
+    if(x == '*' || x == '/') return 2;
+    return 0;
+}
+
 int main(){
-   struct node *head=NULL;
-   struct node *temp;
-   struct node *newnode;
-   
-int i;
-for(i=1;i<=3;i++) {
-   newnode =(struct node*)malloc(sizeof(struct node));
-   printf("Enter data: ");
-   scanf("%d",&newnode->data);
 
-   newnode->link=NULL;
+    char infix[100], postfix[100];
+    int i, j = 0;
 
-   if(head==NULL){
-      head=newnode;
-      temp=newnode;
-   }
-   else {
-      temp->link=newnode;
-      temp=newnode;
-   
-   }
-}
-printf("Linked list:");
-for(temp=head;temp !=NULL;temp = temp->link) {
-   printf("%d",temp->data);
-}
-return 0;
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    for(i = 0; infix[i] != '\0'; i++){
+
+        if(isalnum(infix[i])){  
+            postfix[j++] = infix[i];
+        }
+
+        else if(infix[i] == '('){
+            push(infix[i]);
+        }
+
+        else if(infix[i] == ')'){
+            while(stack[top] != '('){
+                postfix[j++] = pop();
+            }
+            pop();
+        }
+
+        else{
+            while(top != -1 && precedence(stack[top]) >= precedence(infix[i])){
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+    }
+
+    while(top != -1){
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("Postfix expression: %s", postfix);
+
+    return 0;
 }
